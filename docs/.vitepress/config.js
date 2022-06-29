690910f5e057f44292e1f717b3fcceb6c2c53f34
liusai <961931945@qq.com>
2022-06-29 14:05:30 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'hello',
+  description: 'desc',
+  themeConfig: {
+    siteTitle: 'site title',
+    nav: [
+      {text: 'js', link: '/js'},
+      {text: 'vue', link: '/vue'},
+      {text: 'typescript', link: '/typescript'},
+    ],
+  },
+}
