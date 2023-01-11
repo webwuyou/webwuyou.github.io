2f2fc2c7fb49a910e6b8d808be46e0cb12e1b707
webwuyou <961931945@qq.com>
2023-01-11 01:34:52 +0000

    Deploying to gh-pages from  @ 42a8b19c034156ef3f305e15a971a44d42b9bfea 🚀

--- assets/getting-started.md.e4add406.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a}from"./app.ac834f24.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"getting-started.md","lastUpdated":1673319950000}'),r={name:"getting-started.md"};function s(n,o,c,d,i,p){return a(),e("div")}var f=t(r,[["render",s]]);export{m as __pageData,f as default};
---
+++ assets/nuxt_nuxt3_index.md.037d265e.js
@@ -0,0 +1,32 @@
+import{_ as s,c as n,o as a,a as p}from"./app.7d9e6929.js";const D=JSON.parse('{"title":"nuxt3\u4E2D\u7684\u76EE\u5F55\u7ED3\u6784","description":"","frontmatter":{},"headers":[],"relativePath":"nuxt/nuxt3/index.md","lastUpdated":1673347162000}'),l={name:"nuxt/nuxt3/index.md"},e=p(`<h1 id="nuxt3\u4E2D\u7684\u76EE\u5F55\u7ED3\u6784" tabindex="-1">nuxt3\u4E2D\u7684\u76EE\u5F55\u7ED3\u6784 <a class="header-anchor" href="#nuxt3\u4E2D\u7684\u76EE\u5F55\u7ED3\u6784" aria-hidden="true">#</a></h1><div class="language-"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">\u251C\u2500.gitignore</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500app.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500nuxt.config.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500package-lock.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500package.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500pnpm-lock.yaml</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500README.md</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500tsconfig.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500utils</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u251C\u2500request.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u2514time.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500public</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u2514sarah-dayan.jpg</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500pages</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u251C\u2500index.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u251C\u2500article</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   |    \u251C\u2500index.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   |    \u2514[id].vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u251C\u2500about</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   |   \u2514index.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500middleware</span></span>
+<span class="line"><span style="color:#A6ACCD;">|     \u251C\u2500customer.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">|     \u2514default.global.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500layouts</span></span>
+<span class="line"><span style="color:#A6ACCD;">|    \u251C\u2500about.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">|    \u2514default.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500components</span></span>
+<span class="line"><span style="color:#A6ACCD;">|     \u2514a.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500assets</span></span>
+<span class="line"><span style="color:#A6ACCD;">|   \u2514sarah-dayan.jpg</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500.nuxt</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,2),o=[e];function c(t,A,i,r,C,d){return a(),n("div",null,o)}var u=s(l,[["render",c]]);export{D as __pageData,u as default};
